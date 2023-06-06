81f84dd278151af7bd606edf362303488a0a8a5c
AminduBhashana <waamidu@gmail.com>
2023-06-06 21:53:43 +0530

    First Commit

---
+++ InClassPractise/Calc.js
@@ -0,0 +1,9 @@
+let num1 ;
+let num2 ;
+let result;
+num1 = prompt("Enter your First Number : ");
+console.log("First Number : "+num1);
+num2 = prompt("Enter your Second Number : ");
+console.log("Second Number : "+num2);
+result = parseInt(num1)+parseInt(num2);
+console.log("Summation of 2 numbers : "+result);
---
+++ InClassPractise/test1.js
@@ -0,0 +1,23 @@
+console.log("university "+" Kelaniya "+2023);
+
+console.log(10%3);
+console.log(2**3);
+
+let number1,number2;
+number1 = prompt("Enter your number1 : ");
+number2 = prompt("Enter your number2 : ");
+let remindar = (number1 % number2);
+let exp = (number1**number2);
+console.log("Reminder = "+remindar);
+console.log("Exponentiation = "+exp);
+
+console.log(1+3*4-2);
+
+console.log((1+3)*(4-2));
+
+console.log(1*(2/1)*7+5);
+
+console.log("Hello,"+"World!");
+console.log((-1)*(-10)*(-20));
+console.log(2*(4+5*(6+3)));
+console.log(36/3+4*3);
